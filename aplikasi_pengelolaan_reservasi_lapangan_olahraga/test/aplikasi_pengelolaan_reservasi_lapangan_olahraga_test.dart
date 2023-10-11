3f6225aa77737dd04d33d70df675a8cf73a2f45a
ferzian <114915665+ferzian@users.noreply.github.com>
2023-10-11 10:21:26 +0700

    menambah file
    -

---
+++ aplikasi_pengelolaan_reservasi_lapangan_olahraga/test/aplikasi_pengelolaan_reservasi_lapangan_olahraga_test.dart
@@ -0,0 +1,8 @@
+import 'package:aplikasi_pengelolaan_reservasi_lapangan_olahraga/aplikasi_pengelolaan_reservasi_lapangan_olahraga.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
