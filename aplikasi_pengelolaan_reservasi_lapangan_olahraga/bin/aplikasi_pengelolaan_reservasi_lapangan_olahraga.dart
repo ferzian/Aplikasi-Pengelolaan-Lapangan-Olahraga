751f04072c6933fcd3035937185e174f4033cef4
ferzian <114915665+ferzian@users.noreply.github.com>
2023-10-31 13:17:08 +0700

    edit
    mengganti menjadi nomor pelanggan, menambah daftar pelanggan di menu hapus pelanggan.

--- aplikasi_pengelolaan_reservasi_lapangan_olahraga/bin/aplikasi_pengelolaan_reservasi_lapangan_olahraga.dart
+++ aplikasi_pengelolaan_reservasi_lapangan_olahraga/bin/aplikasi_pengelolaan_reservasi_lapangan_olahraga.dart
@@ -24,7 +24,6 @@ class Bayar {
 
   Bayar(this.lapangan, this.durasiSewa);
 
-  
   int totalBayar() {
     return lapangan.hargaLapangan * durasiSewa;
   }
@@ -36,22 +35,30 @@ class Pelanggan {
   Bayar sewa;
 
   Pelanggan(this.nama, this.noTelp, this.sewa);
-
-  
-  void tampilkanInfo() {
-    print('=' * 30);
-    print('            STRUK');
-    print('=' * 30);
-    print("Nama Pelanggan : $nama");
-    print("No. Telepon    : $noTelp");
-    sewa.lapangan.hargaSewa();
-    print("Durasi Sewa    : ${sewa.durasiSewa} jam");
-    print("Total Bayar    : ${sewa.totalBayar()}");
-    print('=' * 30);
-  }
 }
 
-void Inputan(Lapangan lapangan) {
+void LihatDaftarPelanggan(List<Pelanggan> pelangganList) {
+  if (pelangganList.isEmpty) {
+    print("Tidak ada data pelanggan.");
+  } else {
+    print("=== Daftar Pelanggan ===");
+    for (int i = 0; i < pelangganList.length; i++) {
+      print("Nomor pelanggan : ${i + 1}");
+      print("Nama: ${pelangganList[i].nama}");
+      print("No. Telepon: ${pelangganList[i].noTelp}");
+      pelangganList[i].sewa.lapangan.hargaSewa();
+      print("Durasi Sewa: ${pelangganList[i].sewa.durasiSewa} jam");
+      print("Total Bayar: ${pelangganList[i].sewa.totalBayar()}");
+      print("========================");
+    }
+  }
+
+  // print("Tekan Enter untuk kembali ke menu utama.");
+  // stdin.readLineSync(); // Menunggu masukan Enter
+  // clearScreen();
+}
+
+void Inputan(Lapangan lapangan, List<Pelanggan> pelangganList) {
   stdout.write("Masukkan Nama : ");
   var namaPelanggan = stdin.readLineSync()!;
   stdout.write("Masukkan no. telepon : ");
@@ -62,38 +69,78 @@ void Inputan(Lapangan lapangan) {
 
   var sewa = Bayar(lapangan, jamSewa);
   var pelanggan = Pelanggan(namaPelanggan, noTelpPelanggan, sewa);
+  pelangganList.add(pelanggan); // Menambahkan pelanggan ke daftar
+}
 
-  
-  pelanggan.tampilkanInfo();
+void HapusPelanggan(List<Pelanggan> pelangganList, int index) {
+  if (index >= 0 && index < pelangganList.length) {
+    print("Menghapus nomor pelanggan ${index + 1}");
+    pelangganList.removeAt(index);
+  } else {
+    print("Indeks tidak valid.");
+  }
 }
 
 void clearScreen() {
-  print('\x1B[2J\x1B[H'); 
+  print('\x1B[2J\x1B[H');
 }
 
 void main() {
-  print('=' * 20);
-  print('   PILIH LAPANGAN');
-  print('=' * 20);
-  print('1. Lapangan Indoor');
-  print('2. Lapangan Outdoor');
-  print('=' * 20);
-  print('       OPEN');
-  print('   09:00 - 21:00');
-  print('=' * 20);
-  String pilihLapangan = stdin.readLineSync()!.toString();
-  clearScreen();
-  switch (pilihLapangan) {
-    case '1':
-      var LI = LapanganIndoor(150000);
-      Inputan(LI);
-      break;
-    case '2':
-      var LO = LapanganOutdoor(100000);
-      Inputan(LO);
-      break;
-      default:
-  }
+  var pelangganList = <Pelanggan>[]; // Daftar pelanggan
+  bool selesai = false;
 
-  
+  while (!selesai) {
+    print('=' * 25);
+    print('       PILIH MENU');
+    print('=' * 25);
+    print('1. Pesan Lapangan');
+    print('2. Lihat Daftar Pelanggan');
+    print('3. Hapus Pelanggan');
+    print('4. Selesai');
+    print('=' * 25);
+    String pilihMenu = stdin.readLineSync()!.toString();
+    clearScreen();
+    switch (pilihMenu) {
+      case '1':
+    print('=' * 20);
+        print('   PILIH LAPANGAN');
+    print('=' * 20);
+        print('1. Lapangan Indoor');
+        print('2. Lapangan Outdoor');
+    print('=' * 20);
+        print('');
+        String pilihLapangan = stdin.readLineSync()!.toString();
+        clearScreen();
+        switch (pilihLapangan) {
+          case '1':
+            var LI = LapanganIndoor(150000);
+            Inputan(LI, pelangganList);
+            break;
+          case '2':
+            var LO = LapanganOutdoor(100000);
+            Inputan(LO, pelangganList);
+            break;
+          default:
+            print("Pilihan lapangan tidak valid.");
+        }
+        break;
+      case '2':
+        LihatDaftarPelanggan(pelangganList);
+          print("Tekan Enter untuk kembali ke menu utama.");
+  stdin.readLineSync(); // Menunggu masukan Enter
+  clearScreen();
+        break;
+      case '3':
+        LihatDaftarPelanggan(pelangganList);
+        stdout.write("Masukkan nomor pelanggan yang akan dihapus: ");
+        var index = int.parse(stdin.readLineSync()!);
+        HapusPelanggan(pelangganList, index - 1);
+        break;
+      case '4':
+        selesai = true;
+        break;
+      default:
+        print("Pilihan tidak valid.");
+    }
+  }
 }
